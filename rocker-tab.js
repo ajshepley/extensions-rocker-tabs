0b8209619c32dac325e0fc7b14d2eebdc1e695e9
Adam Shepley <ajshepley@gmail.com>
2017-08-13 02:25:30 -0400

    Committing other changes.

--- rocker-tab.js
+++ rocker-tab.js
@@ -2,12 +2,30 @@ document.body.style.border = "5px solid blue";
 
 var middleWasDown = false;
 
-function goToNextTab() {
-    console.log("Go to next tab.");
+// function goToNextTab(currentTabInfo) {
+//     console.log("in goto next");
+//     console.log("Current tab info is ", currentTabInfo);
+
+//     browser.windows.getCurrent({populate: true}).then((windowInfoObj) => {
+//         console.log("Window info obj is ", windowInfoObj);
+        
+//         var currentTabIndex = currentTabInfo.index;
+//         console.log("Go to next tab. Current index is ", currentTabIndex);
+//     });
+// }
+
+// function goToPreviousTab(currentTabInfo) {
+//     console.log("Go to previous tab.");
+// }
+
+function sendNextTabMessage() {
+    console.log("Sending message.");
+    browser.runtime.sendMessage({"tabTarget": "next"});
+    console.log("Message sent.");
 }
 
-function goToPreviousTab() {
-    console.log("Go to previous tab.");
+function sendPreviousTabMessage() {
+    browser.runtime.sendMessage({"tabTarget": "previous"});
 }
 
 document.addEventListener("mousedown", function (event) {
@@ -19,11 +37,26 @@ document.addEventListener("mousedown", function (event) {
     console.log("Middle status is currently: ");
     console.log(middleWasDown);
 
+    console.log("Browser is", browser);
+    console.log("tabs is ", browser.tabs);
+
+    // browser.tabs.getCurrent().then(onGot, onError).catch(function(error) {
+    //     console.log ("aaaaah");
+    //     console.error(error);
+    // });
+
+    console.log("welp")
+
     switch (event.which) {
         case 1:
             console.log('left');
             if (middleWasDown == true) {
-                goToPreviousTab();
+                // var gettingCurrent = browser.tabs.getCurrent();
+                // console.log("What even");
+                // gettingCurrent.then(goToPreviousTab, onError);
+                // getCurrentTabPromise.then(goToPreviousTab);
+                console.log("calling background");
+                sendPreviousTabMessage();
             }
             
             break;
@@ -42,7 +75,8 @@ document.addEventListener("mousedown", function (event) {
         case 3:
             console.log('right');
             if (middleWasDown == true) {
-                goToNextTab();
+                console.log("Promise time.");
+                sendNextTabMessage();
             }
             break;
     }
